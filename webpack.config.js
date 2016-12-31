65d6c0175b70446f7776280f4722fc3284452017
dewittsc <scott@terescode.com>
2016-12-31 15:17:07 -0500

    Cleaned up webpack config and added extract text webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,8 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ["react", "es2015"],
-          plugins: ["transform-object-rest-spread"]
+          presets: ['react', 'es2015'],
+          plugins: ['transform-object-rest-spread']
         }
       }
     ]
